f0a97c253bd954c855fb80bf67ac8272c4f78fb5
Elazar <96201125+awakzdev@users.noreply.github.com>
2022-06-02 17:29:13 +0300

    Added btn - comment page redirection

--- movie/urls.py
+++ movie/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('language/<str:lang>', MovieLanguage.as_view(), name='movie_language'),
     path('year/<int:year>', MovieYear.as_view(), name='movie_year'),
     path('<slug:slug>', MovieDetail.as_view(), name='movie_detail'),
-    path('<slug:slug>/add-comment/', views.add_comment, name='add-comment'),
+    path('<slug:slug>/add-comment/', views.add_comment, name='add_comment'),
 ]
 
