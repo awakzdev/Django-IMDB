be7061209cd481598449db32a6a8e14a0023fd60
Elazar <96201125+awakzdev@users.noreply.github.com>
2022-06-26 16:57:46 +0300

    Update settings.py

--- imdb/settings.py
+++ imdb/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -129,6 +130,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
