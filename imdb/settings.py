748e539b3745f8db29d6c0d78ac56a1369489290
Elazar <96201125+awakzdev@users.noreply.github.com>
2022-06-26 16:57:58 +0300

    Update settings.py

--- imdb/settings.py
+++ imdb/settings.py
@@ -128,6 +128,3 @@ STATIC_URL = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
